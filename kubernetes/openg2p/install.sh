a165bd69a3f0cbb01812168c4ecac3d1b1bffc00
Lalith Kota <kotalalith@gmail.com>
2023-07-29 01:26:53 +0530

    Changed Helm urls in openg2p scripts

--- kubernetes/openg2p/install.sh
+++ kubernetes/openg2p/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NS=odk
+NS=openg2p
 
 helm repo add openg2p https://openg2p.github.io/openg2p-helm
 helm repo update
@@ -8,4 +8,4 @@ helm repo update
 echo Create $NS namespace
 kubectl create ns $NS
 
-helm -n $NS install odk-central openg2p/odk-central  $@
+helm -n $NS install openg2p openg2p/openg2p  $@
