908ae5f5249a8cbeed7b5a3b707362cfaf127a6f
Lalith Kota <kotalalith@gmail.com>
2023-12-21 23:45:59 +0530

    GCTB: Added default fa backend mapping

--- kubernetes/social-payments-account-registry/install.sh
+++ kubernetes/social-payments-account-registry/install.sh
@@ -9,6 +9,6 @@ kubectl create ns $NS
 kubectl -n $NS delete cm mapper-registry-schemas --ignore-not-found=true
 kubectl -n $NS create cm mapper-registry-schemas --from-file=schemas/FinancialAddressMapper.json
 
-helm -n $NS install spar-self-service-ui openg2p/spar-self-service-ui
+helm -n $NS install spar-self-service-ui openg2p/spar-self-service-ui $@
 
 helm -n $NS install spar openg2p/social-payments-account-registry -f values.yaml --wait $@
