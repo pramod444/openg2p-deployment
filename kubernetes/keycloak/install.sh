a1bc771714f970c7cbe8f743a54b4c1fc8be62b5
Lalith Kota <kotalalith@gmail.com>
2024-01-21 16:55:49 +0530

    Added Superset keycloak client

--- kubernetes/keycloak/install.sh
+++ kubernetes/keycloak/install.sh
@@ -35,6 +35,7 @@ if [ "$KEYCLOAK_INIT_ENABLED" != "false" ]; then
   export OPENG2P_MINIO_CLIENT_SECRET=$(kubectl -n $NS get secret keycloak-client-secrets -o jsonpath={.data.openg2p_minio_client_secret} | base64 --decode)
   export OPENG2P_KAFKA_CLIENT_SECRET=$(kubectl -n $NS get secret keycloak-client-secrets -o jsonpath={.data.openg2p_kafka_client_secret} | base64 --decode)
   export OPENG2P_KIBANA_CLIENT_SECRET=$(kubectl -n $NS get secret keycloak-client-secrets -o jsonpath={.data.openg2p_kibana_client_secret} | base64 --decode)
+  export OPENG2P_SUPERSET_CLIENT_SECRET=$(kubectl -n $NS get secret keycloak-client-secrets -o jsonpath={.data.openg2p_superset_client_secret} | base64 --decode)
 
   envsubst \
     '${KEYCLOAK_HOSTNAME}
@@ -43,7 +44,8 @@ if [ "$KEYCLOAK_INIT_ENABLED" != "false" ]; then
     ${OPENG2P_SERVICEPROVIDER_CLIENT_SECRET}
     ${OPENG2P_MINIO_CLIENT_SECRET}
     ${OPENG2P_KAFKA_CLIENT_SECRET}
-    ${OPENG2P_KIBANA_CLIENT_SECRET}' < ${KEYCLOAK_REALM_NAME}-realm.json > /tmp/${KEYCLOAK_REALM_NAME}-realm.json
+    ${OPENG2P_KIBANA_CLIENT_SECRET}
+    ${OPENG2P_SUPERSET_CIENT_SECRET}' < ${KEYCLOAK_REALM_NAME}-realm.json > /tmp/${KEYCLOAK_REALM_NAME}-realm.json
 
   keycloak_import_realm \
     "$(keycloak_get_admin_token)" \
