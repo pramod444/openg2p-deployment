e73d4bbc22c888134ef236d9192bf4a517185910
Puneet Joshi <puneet@openg2p.org>
2024-03-31 10:20:29 +0530

    Previous commit restored for legacy reasons.

---
+++ kubernetes/social-payments-account-registry/README.md
@@ -0,0 +1,3 @@
+## Social Payments Account Registry
+
+- Refer to [SPAR deployment instructions](https://docs.openg2p.org/deployment/openg2p-modules-deployment/spar-deployment)
---
+++ kubernetes/social-payments-account-registry/copy_secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copy secrets from other namespaces
+# DST_NS: Destination namespace
+
+COPY_UTIL=../utils/copy_cm_func.sh
+DST_NS=spar
+$COPY_UTIL secret postgres-postgresql postgres $DST_NS 
+
