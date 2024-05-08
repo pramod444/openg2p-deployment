40fda70091b023eb13b9ade580779c8d0bce84b4
shivamg9 <shivamg@iitkgp@ac.in>
2024-05-08 17:41:43 +0530

    Changed Hostname for beneficiary portal

--- kubernetes/openg2p/install.sh
+++ kubernetes/openg2p/install.sh
@@ -3,6 +3,7 @@
 export SANDBOX_HOSTNAME=${SANDBOX_HOSTNAME:-openg2p.sandbox.net}
 export OPENG2P_MAILNAME=${OPENG2P_MAILNAME:-$SANDBOX_HOSTNAME}
 export OPENG2P_HOSTNAME=${OPENG2P_HOSTNAME:-$SANDBOX_HOSTNAME}
+export OPENG2P_SELFSERVICE_HOSTNAME=${OPENG2P_SELFSERVICE_HOSTNAME:-selfservice.$SANDBOX_HOSTNAME}
 export OPENG2P_SERVICEPROV_HOSTNAME=${OPENG2P_SERVICEPROV_HOSTNAME:-serviceprovider.$SANDBOX_HOSTNAME}
 
 NS=openg2p
@@ -20,7 +21,7 @@ helm -n $NS install openg2p openg2p/openg2p \
     --version ${VERSION} \
     --set global.hostname=$OPENG2P_HOSTNAME \
     --set global.mailName=$OPENG2P_MAILNAME \
-    --set selfServiceIngress.enabled=false \
+    --set global.selfServiceHostname=$OPENG2P_SELFSERVICE_HOSTNAME \
     --set global.serviceProviderHostname=$OPENG2P_SERVICEPROV_HOSTNAME \
     --set odoo.image.repository=${OPENG2P_ODOO_IMAGE_REPO:-openg2p/openg2p-odoo-package} \
     --set odoo.image.tag=${OPENG2P_ODOO_IMAGE_TAG:-15.0-develop} \
