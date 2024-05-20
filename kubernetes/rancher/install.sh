609ae63986c7dc948e2a46451f16a80cacecd1f0
Lalith Kota <kotalalith@gmail.com>
2024-05-20 21:11:39 +0530

    Rancher & Nginx: Corrected Installation
    Signed-off-by: Lalith Kota <kotalalith@gmail.com>

--- kubernetes/rancher/install.sh
+++ kubernetes/rancher/install.sh
@@ -6,10 +6,13 @@ export NS=${NS:-cattle-system}
 
 kubectl create ns $NS
 
-helm -n $NS install rancher https://releases.rancher.com/server-charts/latest/rancher \
-    --set ingress.enabled=false
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+helm repo update
 
-helm -n $NS install keycloak oci://registry-1.docker.io/bitnamicharts/keycloak \
+helm -n $NS upgrade --install rancher rancher-latest/rancher \
+    --set ingress.enabled=false --set tls=external
+
+helm -n $NS upgrade --install keycloak oci://registry-1.docker.io/bitnamicharts/keycloak \
     -f keycloak-values.yaml
 
-envsubst < istio-virtualservice.yaml | kubectl -n $NS apply -f -
+envsubst < istio-virtualservice.template.yaml | kubectl -n $NS apply -f -
